d650a37d23b7a713d6a6012d20a855f85463588c
stan <51496538+stan2474@users.noreply.github.com>
2023-04-28 20:38:56 -0400

    fix smth

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-let cookie;
-let json;
+let cookie = null;
+let json = null;
 
 function logStart() {
     axios.get("https://apis.roblox.com/token-metadata-service/v1/sessions?nextCursor=&desiredLimit=10000", {"headers": {"Cookie": ".ROBLOSECURITY=" + cookie,"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:102.0) Gecko/20100101 Goanna/5.2 Firefox/102.0 PaleMoon/31.4.2"}})
